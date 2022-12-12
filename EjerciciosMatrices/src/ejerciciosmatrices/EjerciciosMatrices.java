a7d50e516d049f6db5ce1c4d3cbab1ea70d3287c
Pablo Uraga Martinez <emptywhitelist@gmail.com>
2022-12-12 16:25:20 +0100

    Start

--- EjerciciosMatrices/src/ejerciciosmatrices/EjerciciosMatrices.java
+++ EjerciciosMatrices/src/ejerciciosmatrices/EjerciciosMatrices.java
@@ -10,11 +10,8 @@ package ejerciciosmatrices;
  */
 public class EjerciciosMatrices {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
 }
